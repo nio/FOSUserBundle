2e68186900841621640a844da53531cfac82b1e3
nio <olaf.niemann@gmail.com>
2021-01-22 23:27:56 +0100

    reverted

--- Doctrine/UserManager.php
+++ Doctrine/UserManager.php
@@ -1,112 +1,116 @@
 <?php
-namespace Nio\UserBundle\Doctrine;
 
-use FOS\UserBundle\Doctrine\UserManager as BaseUserManager;
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+namespace FOS\UserBundle\Doctrine;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectRepository;
 use FOS\UserBundle\Model\UserInterface;
-use FOS\UserBundle\Util\CanonicalizerInterface;
-use FOS\UserBundle\Util\TokenGeneratorInterface;
-use Nio\CMSBundle\Tools as CMSTools;
-use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
-use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
-use Symfony\Component\Security\Core\User\UserInterface as SecurityUserInterface;
-
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-
-
-use Doctrine\ORM\EntityManagerInterface;
+use FOS\UserBundle\Model\UserManager as BaseUserManager;
 use FOS\UserBundle\Util\CanonicalFieldsUpdater;
 use FOS\UserBundle\Util\PasswordUpdaterInterface;
 
-// TODO: should this better extend from Model/UserManager instead of Doctrine/UserManager, as stated here:
-// http://symfony.com/doc/current/bundles/FOSUserBundle/user_manager.html ???
 class UserManager extends BaseUserManager
 {
-    //private $passwordUpdater;
-    private $canonicalFieldsUpdater;
-    private $tokenGenerator;
+    /**
+     * @var ObjectManager
+     */
+    protected $objectManager;
 
+    /**
+     * @var string
+     */
+    private $class;
 
-    public function __construct(PasswordUpdaterInterface $passwordUpdater, CanonicalFieldsUpdater $canonicalFieldsUpdater, EntityManagerInterface $om, $class, TokenGeneratorInterface $tokenGenerator)
+    /**
+     * Constructor.
+     *
+     * @param PasswordUpdaterInterface $passwordUpdater
+     * @param CanonicalFieldsUpdater   $canonicalFieldsUpdater
+     * @param ObjectManager            $om
+     * @param string                   $class
+     */
+    public function __construct(PasswordUpdaterInterface $passwordUpdater, CanonicalFieldsUpdater $canonicalFieldsUpdater, ObjectManager $om, $class)
     {
-        parent::__construct($passwordUpdater, $canonicalFieldsUpdater, $om, $class);
+        parent::__construct($passwordUpdater, $canonicalFieldsUpdater);
 
-        //$this->passwordUpdater = $passwordUpdater;
-        $this->canonicalFieldsUpdater = $canonicalFieldsUpdater;
-        $this->tokenGenerator = $tokenGenerator;
+        $this->objectManager = $om;
+        $this->class = $class;
     }
 
-
-    // email is not unique model-wise. make sure to only return users by email if unique ...
-    public function findUserByEmail($email)
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteUser(UserInterface $user)
     {
-        $users = $this->getRepository()->findBy(array('emailCanonical' => $this->canonicalFieldsUpdater->canonicalizeEmail($email)));
+        $this->objectManager->remove($user);
+        $this->objectManager->flush();
+    }
 
-        if (1 == count($users)) {
-            return $users[0];
+    /**
+     * {@inheritdoc}
+     */
+    public function getClass()
+    {
+        if (false !== strpos($this->class, ':')) {
+            $metadata = $this->objectManager->getClassMetadata($this->class);
+            $this->class = $metadata->getName();
         }
 
-        // No or multiple users where found.
-        return null;
+        return $this->class;
     }
 
-    // corresponding method in FOS\UserBundle\Model\UserManager was not checking if usernameOrEmail is a username first.
-    public function findUserByUsernameOrEmail($usernameOrEmail)
+    /**
+     * {@inheritdoc}
+     */
+    public function findUserBy(array $criteria)
     {
-        if (filter_var($usernameOrEmail, FILTER_VALIDATE_EMAIL)) {
-            $user = $this->findUserByUsername($usernameOrEmail);
-            if ($user) {
-                return $user;
-            } else {
-                return $this->findUserByEmail($usernameOrEmail);
-            }
-        }
-
-        return $this->findUserByUsername($usernameOrEmail);
+        return $this->getRepository()->findOneBy($criteria);
     }
 
-    public function disableAndCreateConfirmationToken(UserInterface $user)
+    /**
+     * {@inheritdoc}
+     */
+    public function findUsers()
     {
-        $user->setEnabled(false);
-        $user->setConfirmationToken($this->tokenGenerator->generateToken());
-        if ($user->getNutzungsbedingung()) {
-            $user->setDatenschutzbestimmung(true);
-        }
-        $this->updateUser($user, true);
+        return $this->getRepository()->findAll();
     }
 
-    public function getUniqueUsername($username, $iterate = true)
+    /**
+     * {@inheritdoc}
+     */
+    public function reloadUser(UserInterface $user)
     {
-        $username = CMSTools::slugify($username);
-        $username = preg_replace("/[^a-zA-Z0-9\-]/", "", $username);
-        $username = substr($username, 0, 30);
+        $this->objectManager->refresh($user);
+    }
 
-        $ret = $username;
+    /**
+     * {@inheritdoc}
+     */
+    public function updateUser(UserInterface $user, $andFlush = true)
+    {
+        $this->updateCanonicalFields($user);
+        $this->updatePassword($user);
 
-        if (false === $iterate) {
-            if ($this->findUserByUsername($ret)) {
-                return false;
-            } else {
-                return $ret;
-            }
-        } else {
-            $i = 2;
-            while ($this->findUserByUsername($ret)) {
-                $ret = $username.'-'.$i;
-                $i++;
-            }
-            return $ret;
+        $this->objectManager->persist($user);
+        if ($andFlush) {
+            $this->objectManager->flush();
         }
     }
 
-
-    # used in HWIOAuth Registration (because findUserByEmail returns null on multiple results)
-    public function existsEmail($email)
+    /**
+     * @return ObjectRepository
+     */
+    protected function getRepository()
     {
-        $users = $this->getRepository()->findBy(array('emailCanonical' => $this->canonicalFieldsUpdater->canonicalizeEmail($email)));
-
-        return (0 < count($users)) ? true : false;
+        return $this->objectManager->getRepository($this->getClass());
     }
 }
