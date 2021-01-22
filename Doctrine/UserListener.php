6fd3cd72fc0779563ffa09b168b2a37b6ad4f532
nio <olaf.niemann@gmail.com>
2021-01-22 23:50:14 +0100

    sf52

--- Doctrine/UserListener.php
+++ Doctrine/UserListener.php
@@ -13,7 +13,7 @@ namespace FOS\UserBundle\Doctrine;
 
 use Doctrine\Common\EventSubscriber;
 //use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
-use Doctrine\ORM\Event\LifecycleEventArgs
+use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 //use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ODM\MongoDB\DocumentManager;
